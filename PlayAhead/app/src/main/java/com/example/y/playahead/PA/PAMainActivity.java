35f99ca784a470e5bdaddf6aa792607450691c22
xiaobanni <xjc5069@163.com>
2019-03-13 17:48:26 +0800

    add some button

--- PlayAhead/app/src/main/java/com/example/y/playahead/PA/PAMainActivity.java
+++ PlayAhead/app/src/main/java/com/example/y/playahead/PA/PAMainActivity.java
@@ -50,6 +50,9 @@ public class PAMainActivity extends Activity {
             }
         });
 
+
+
+
         bt_setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
